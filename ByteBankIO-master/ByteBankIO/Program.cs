a736c19752f05851c11647a5609640acc0eb93ce
Guilherme Bueno <53920713+UrsaoPixel@users.noreply.github.com>
2023-02-20 15:23:26 -0300

    Add files via upload

---
+++ ByteBankIO-master/ByteBankIO/Program.cs
@@ -0,0 +1,28 @@
+﻿using ByteBankIO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        var linhas = File.ReadAllLines("contas.txt");
+        Console.WriteLine(linhas.Length);
+
+        /*
+        foreach (var linha in linhas) 
+        {
+            Console.WriteLine(linha);
+        }
+        */
+
+        var bytesArquivo = File.ReadAllBytes("contas.txt");
+        Console.WriteLine($"Arquivo contas.txt está com {bytesArquivo.Length} bytes");
+
+        File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllTexte");
+        
+        Console.WriteLine("Aplicação Finalizada ...");
+
+        Console.ReadLine();
+    }
+}
